f34d380ad838f585fb344b331bd812541edc4a5a
EleazarGomez <elithegolfer@gmail.com>
2019-09-26 16:25:40 -0700

    Code for all events

--- SRC/DT_WS.py
+++ SRC/DT_WS.py
@@ -1,8 +1,10 @@
-# WARNING: Do not rename this file "websocket.py"
+# WARNING: Do not rename this file "websocket.py".
+# NOTE/TODO: Currently BICYCLE and METROLOGY events don't work.
+# NOTE/TODO: If the socket sends more than 3 units of bytes without
+#            retrieving events, the code is not working at this time.
+#            Try again tomorrow.
 
 import os
-import time
-import _thread
 
 # pip install websocket-client
 import websocket
@@ -33,18 +35,35 @@ def on_close(ws):
 def on_open(ws):
     print('### connected ###')
 
-    # Example for Traffic Predix Zone
-    ws.send('{"bbox":"-90:-180,90:180","eventTypes":["TFEVT"]}')
+    # The events are listed in this order:
+    #   TRAFFIC
+    #   PARKING
+    #   BICYCLE
+    #   PEDESTRIAN
+    #   METROLOGY
+    #   ENVIRONMENTAL
 
-    # Example for Parking Predix Zone
-    # ws.send('{"bbox":"-90:-180,90:180","eventTypes":["PKIN","PKOUT"]}')
+    #ws.send('{"bbox":"-90:-180,90:180","eventTypes":["TFEVT"]}')
+    #ws.send('{"bbox":"-90:-180,90:180","eventTypes":["PKIN","PKOUT"]}')
+    #ws.send('{"bbox":"-90:-180,90:180","eventTypes":["BICYCLE"]}')
+    #ws.send('{"bbox":"-90:-180,90:180","eventTypes":["PEDEVT"]}')
+    #ws.send('{"bbox":"-90:-180,90:180","eventTypes":["METROLOGY"]}')
+    ws.send('{"bbox":"-90:-180,90:180","eventTypes":["PRESSURE","TEMPERATURE","ORIENTATION","HUMIDITY"]}')
 
 if __name__ == '__main__':
     websocket.enableTrace(True)
 
-    # Use Predix-Zone-ID to match events of interest
-    # cityiq_zone = '---Parking Predix Zone HERE ---'
-    cityiq_zone = 'SD-IE-TRAFFIC'
+    # DIRECTIONS:
+    #
+    # Uncomment/Comment corresponding lines here and in the "on_open"
+    # function to try a different web socket.
+    
+    #cityiq_zone = 'SD-IE-TRAFFIC'
+    #cityiq_zone = 'SD-IE-PARKING'
+    #cityiq_zone = 'SD-IE-BICYCLE'
+    #cityiq_zone = 'SD-IE-PEDESTRIAN'
+    #cityiq_zone = 'SD-IE-METROLOGY'
+    cityiq_zone = 'SD-IE-ENVIRONMENTAL'
 
     cityiq = 'wss://sandiego.cityiq.io/api/v2/websocket/events'
     token = get_auth_token()
